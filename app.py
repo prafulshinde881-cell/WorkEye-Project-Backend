2bc3f1231c303874546db36850b7daa00fcb8916
Shivam Khaw <prafulshinde881@email.com>
2026-03-04 13:45:09 +0530

    app.py

--- app.py
+++ app.py
@@ -215,5 +215,5 @@ def root():
 # ======================================================================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host="0.0.0.0", port=port, debug=False)
